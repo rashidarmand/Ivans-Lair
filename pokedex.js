481f97dad19192fbcbe4773868feca94357e7d7a
Rashid H Armand <rashidharmand@gmail.com>
2018-04-01 16:23:44 -0400

    Finished JS for AJAX calls to the pokemon API. Added comments as well to explain what is going on

--- pokedex.js
+++ pokedex.js
@@ -1,81 +1,169 @@
+// Creates the Trainer class with two methods: add() & get(name)
 class Trainer {
+
     constructor() {
         this.party = [];
     }
 
-    all(pokemon) {
-        // return back a list of all pokemon
-
-        this.party.push(pokemon);
-
-
-        // let listOfAllPokemon = document.querySelectorAll('Pokemon');
-        // return listOfAllPokemon;
+    all() {
+        // Return back a list of all pokemon objects in the 'this.party' array
+        console.log(this.party);
     }
 
     get(name) {
-        // return just one pokemon
-        return this.Pokemon;
+        // Return just one pokemon object based on a name being passed as a string. Not case-sensitive.
+        let index = 0
+        for (index of this.party) {
+            if (name.toLowerCase() === index.name){
+                console.log(index);
+            }
+        }
     }
+
 }
 
+// Creates a new Trainer object named 'Ivan'
+let ivan = new Trainer()
 
-
-let Ivan = new Trainer().all()
-
-console.log(Ivan)
-
-
+// Creates the Pokemon class and defines the parameters that it accepts
 class Pokemon {
-    constructor(name, hp, attack, defense, abilities) {
+    constructor(name, hp, attack, defense, specialAttack, specialDefense, abilities) {
         this.name = name;
         this.hp = hp;
         this.attack = attack;
         this.defense = defense;
+        this.specialAttack = specialAttack;
+        this.specialDefense = specialDefense;
         this.abilities = abilities;
     }
 }
 
-let bulba = new Pokemon("bulba", 400, 500, 600, ['blow shit up', 'fuck shit up', 'beat shit up'])
-let chara = new Pokemon("chara", 400, 500, 600, ['blow shit up', 'fuck shit up', 'beat shit up'])
-let squida = new Pokemon("squida", 400, 500, 600, ['blow shit up', 'fuck shit up', 'beat shit up'])
 
-console.log(bulba)
-console.log(chara)
-console.log(squida)
+// Get 1st Pokemon's information from the API - 'Mewtwo'
+axios.get(`https://pokeapi.co/api/v2/pokemon/150`)
+.then(function (response) {
+    // Loops through the selected pokemon's abilities in the JSON file and pushes it to an empty array.
+    // The array is then returned so I can access the data inside of it at a later point.
+    let getAbilities = (()=> {
+          let array = []
+          let i = 0
+          while(i < response.data.abilities.length) {
+              array.push(response.data.abilities[i].ability['name']);
+              i++;
+          } 
+          return array;
+      })
 
+    let name = response.data['name']  // Name
+    let hp = response.data.stats[5]['base_stat']  // HP
+    let attack = response.data.stats[4]['base_stat']  // Attack
+    let defense = response.data.stats[3]['base_stat'] // Defense
+    let specialAttack = response.data.stats[2]['base_stat']   // Special-Attack
+    let specialDefense = response.data.stats[1]['base_stat']  // Special-Defense
+    let abilities = getAbilities()    // Calls the getAbilites function which returns an array with the names of the abilites
 
-let section = document.querySelector('#show');
-
-axios.get('https://pokeapi.co/api/v2/pokemon/150/')
-  .then(function (response) {
-      let data = response.data;
-      let arr = []
+    // Create a new Pokemon object with the name 'Mewtwo' and pass it data from the AJAX call.
+    let mewtwo = new Pokemon (
+        name,
+        hp,
+        attack,
+        defense,
+        specialAttack,
+        specialDefense,
+        abilities
+    )
     
-    //   console.log(data);
+    // Adds the new Pokemon to the end of the array in my Trainer object so it can be accessed later on with either the .get() or the .all() method
+    ivan.party.push(mewtwo)
+})
+.catch(function (error) {
+  console.log(error);
+});
 
-    console.log(response.data['name']); //name
-    console.log(response.data.stats[5]['base_stat']); //hp
-    console.log(response.data.stats[4]['base_stat']); //attack
-    console.log(response.data.stats[2]['base_stat']); //special-attack
-    console.log(response.data.stats[3]['base_stat']); //defense
-    console.log(response.data.stats[1]['base_stat']); //special-defense
-    
-    //data.abilities.forEach((ability)=> {
-        let theAbilities = response.data.abilities;
-        let i = 0
-        do {
-            arr.push(response.data.abilities[i].ability['name']);           //gets the abilities in an array format.
-            i++;
-            // console.log(arr);
-        }
-        while(i < theAbilities.length)
-        //  console.log(arr);
-    //})
 
-    console.log(arr);
-    
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+// Get the 2nd Pokemon's information from the API - 'Mew'
+axios.get(`https://pokeapi.co/api/v2/pokemon/151`)
+.then(function (response) {
+    // Loops through the selected pokemon's abilities in the JSON file and pushes it to an empty array.
+    // The array is then returned so I can access the data inside of it at a later point.
+    let getAbilities = (()=> {
+          let array = []
+          let i = 0
+          while(i < response.data.abilities.length) {
+              array.push(response.data.abilities[i].ability['name']);
+              i++;
+          } 
+          return array;
+      })
+
+    let name = response.data['name']  // Name
+    let hp = response.data.stats[5]['base_stat']  // HP
+    let attack = response.data.stats[4]['base_stat']  // Attack
+    let defense = response.data.stats[3]['base_stat'] // Defense
+    let specialAttack = response.data.stats[2]['base_stat']   // Special-Attack
+    let specialDefense = response.data.stats[1]['base_stat']  // Special-Defense
+    let abilities = getAbilities()    // Calls the getAbilites function which returns an array with the names of the abilites
+
+    //Create a new Pokemon object with the name 'Mew' and pass it data from the AJAX call.
+    let mew = new Pokemon (
+        name,
+        hp,
+        attack,
+        defense,
+        specialAttack,
+        specialDefense,
+        abilities
+    )
+
+    // Adds the new Pokemon to the end of the array in my Trainer object so it can be accessed later on with either the .get() or the .all() method
+    ivan.party.push(mew)
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
+// Get the 3rd Pokemon's information from the API - 'Rayquaza'
+axios.get(`https://pokeapi.co/api/v2/pokemon/384`)
+.then(function (response) {
+    // Loops through the selected pokemon's abilities in the JSON file and pushes it to an empty array.
+    // The array is then returned so I can access the data inside of it at a later point.
+    let getAbilities = (()=> {
+          let array = []
+          let i = 0
+          while(i < response.data.abilities.length) {
+              array.push(response.data.abilities[i].ability['name']);
+              i++;
+          } 
+          return array;
+      })
+
+    let name = response.data['name']  // Name
+    let hp = response.data.stats[5]['base_stat']  // HP
+    let attack = response.data.stats[4]['base_stat']  // Attack
+    let defense = response.data.stats[3]['base_stat'] // Defense
+    let specialAttack = response.data.stats[2]['base_stat']   // Special-Attack
+    let specialDefense = response.data.stats[1]['base_stat']  // Special-Defense
+    let abilities = getAbilities()    // Calls the getAbilites function which returns an array with the names of the abilites
+
+    //Create a new Pokemon object with the name 'Rayquaza' and pass it data from the AJAX call.
+    let rayquaza = new Pokemon (
+        name,
+        hp,
+        attack,
+        defense,
+        specialAttack,
+        specialDefense,
+        abilities
+    )
+
+    // Adds the new Pokemon to the end of the array in my Trainer object so it can be accessed later on with either the .get() or the .all() method
+    ivan.party.push(rayquaza)
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
+
+
